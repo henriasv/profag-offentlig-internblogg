15c8e299cb0cd458c12fc3851743388e6a6b0287
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2018-12-17 10:14:49 +0100

    first commit

---
+++ convert_notebooks.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+path = "pages"
+currentPath = os.path.dirname(os.path.abspath(__file__))
+
+for dirName, subdirList, fileList in os.walk(path):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        if ".ipynb" in fname: 
+            os.chdir(os.path.join(os.path.dirname(__file__), dirName))
+            subprocess.call(["jupyter", "nbconvert", fname, "--to", "markdown"])
+            os.chdir(currentPath)
